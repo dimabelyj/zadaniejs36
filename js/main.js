d806708d082fe5eddce55be2fcb945167a57bc5f
Dmytro Bilokolodskykh <belokolodskikh@gmail.com>
2019-04-03 16:22:27 +0200

    koniec

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-let x = 1;
-let y = 2;
-x === y;
+function spr(x, y) {
+    let sum = x + y;
+    if (x == 20 || y == 20) {
+        return true;
+    } else if (sum <= 20) {
+        return true;
+    }
+    return false;
+}
+spr(x, y);
